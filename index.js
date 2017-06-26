4e59cedc3c1343c1c38416dba8919907f9387560
Olivia Burris <oliviaburris11@gmail.com>
2017-06-26 14:28:21 -0400

    Add second input and display in paragraph

--- index.js
+++ index.js
@@ -2,13 +2,21 @@
 function updateHeading(ev) {
   ev.preventDefault()
   const f = ev.target
+  const l = ev.target
+  const lastName = l.personLastName.value
   const name = f.personName.value
   const heading = document.querySelector('h1')
   heading.textContent = name
+  
   const divParagraph = document.querySelector('#divP')
-  divParagraph.innerHTML = name
+  divParagraph.innerHTML = (name + " " + lastName)
+
+  //const lastName = personLastName.value
+  //console.log(lastName)
 }
+
 
 const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', updateHeading)
+
 
