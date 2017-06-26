9bf12cbafa7f52185a433f6101d1760f05588cab
Olivia Burris <oliviaburris11@gmail.com>
2017-06-26 15:05:32 -0400

    If blue is typed the output will be the color blue, working on the other colors

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 
 function updateHeading(ev) {
+  
   ev.preventDefault()
   const f = ev.target
-  const l = ev.target
-  const lastName = l.personLastName.value
+ 
+  const lastName = f.personLastName.value
   const name = f.personName.value
-  const heading = document.querySelector('h1')
-  heading.textContent = name
+ // const heading = document.querySelector('h1')
   
   const divParagraph = document.querySelector('#divP')
   divParagraph.innerHTML = (name + " " + lastName)
 
-  //const lastName = personLastName.value
-  //console.log(lastName)
+if (name || lastName === "blue") {
+      document.querySelector('#divP').style.color="blue"
+}
+else if (name || lastName === "red") {
+      document.querySelector('#divP').style.color="red"
+} 
+else {
+    document.querySelector('divP').style.color="black"
+}
+
 }
 
 
